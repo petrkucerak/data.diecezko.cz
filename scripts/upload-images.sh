a8378166c7b3d05d6e85ea93d68a1a3094e45a6a
petrkucerak <petr@khome.cz>
2023-11-27 14:52:00 +0100

    Remove images

---
+++ scripts/upload-images.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Source and destination folders
+source_folder="/../../tmp"
+destination_folder="/../2023/foto"
+
+# Check if source folder exists
+if [ ! -d "$source_folder" ]; then
+   echo "Source folder does not exist."
+   exit 1
+fi
+
+# Check if destination folder exists, if not, create it
+if [ ! -d "$destination_folder" ]; then
+   mkdir -p "$destination_folder"
+fi
+
+# Loop through each file in the source folder
+for file in "$source_folder"/*; do
+   # Check if the item is a file
+   if [ -f "$file" ]; then
+      # Extract the file name from the full path
+      file_name=$(basename "$file")
+
+      # Copy the file to the destination folder
+      cp "$file" "$destination_folder/$file_name"
+
+      echo "Copied: $file_name"
+   fi
+done
+
+echo "Copy process completed."
